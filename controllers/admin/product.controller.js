d13c891467788f92b4558362a85007efee09c2f2
trungtin252 <laitrungtin21@gmail.com>
2024-06-13 17:30:01 +0700

    add uploadCloud

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -114,8 +114,7 @@ module.exports.createPost = async (req, res) => {
     }else{
         req.body.position = parseInt(req.body.position);
     }
-
-    req.body.thumbnail = `/uploads/${req.file.filename}`
+    
     const product = new Product(req.body);
     await product.save();
 
--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,7 +1,14 @@
 const express = require("express")
-const multer  = require('multer')
+const multer = require('multer')
+const cloudinary = require('cloudinary').v2
+const streamifier = require('streamifier')
 // const upload = multer({ dest: './public/uploads/' })
-const upload = require('../../helpers/storeMuler');
+// const upload = require('../../helpers/storeMuler');
+const upload = multer()
+const uploadCloudModule = require('../../middlewares/admin/uploadCloud.middlewares');
+
+
+
 
 const route = express.Router();
 const controller = require('../../controllers/admin/product.controller')
@@ -12,8 +19,12 @@ route.patch("/change-status/:status/:id", controller.changeStatus);
 route.patch("/change-multi", controller.changeMulti);
 route.delete("/delete/:id", controller.deleteItem);
 route.get('/create', controller.create);
-route.post('/create',upload.single("thumbnail"),  controller.createPost);
+
+
+
+route.post('/create', upload.single("thumbnail"), uploadCloudModule.uploadCloud ,controller.createPost
+);
 route.get("/edit/:id", controller.edit);
-route.patch("/edit/:id",upload.single("thumbnail"), controller.editPatch)
+route.patch("/edit/:id", upload.single("thumbnail"), controller.editPatch)
 
 module.exports = route;
